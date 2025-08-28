c3a7de2c410c94d932c4961ff098cad29f0aa985
Natali Korban <natafuuuck@gmail.com>
2025-08-28 16:14:23 +0300

    add  field 11 12 13

---
+++ hw07-structurualPatterns/src/main/java/ru/otus/processor/homework/forHomework.java
@@ -0,0 +1,17 @@
+package ru.otus.processor.homework;
+
+import ru.otus.model.Message;
+
+public class ProcessorSwapFields11And12 implements Processor {
+
+    @Override
+    public Message process(Message message) {
+        var field11 = message.getField11();
+        var field12 = message.getField12();
+
+        return message.toBuilder()
+                .field11(field12)
+                .field12(field11)
+                .build();
+    }
+}
