23ea457f7fe6cbac454b4745e18b65035a3dd35d
Natali Korban <natafuuuck@gmail.com>
2025-08-29 14:33:41 +0300

    правки по комметариям

--- hw07-structurualPatterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-structurualPatterns/src/main/java/ru/otus/HomeWork.java
@@ -2,10 +2,11 @@ package ru.otus;
 
 import java.time.Clock;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.handler.ComplexProcessor;
-import ru.otus.listener.homework.MessageHistoryListener;
+import ru.otus.listener.homework.HistoryListener;
 import ru.otus.model.Message;
 import ru.otus.model.ObjectForMessage;
 import ru.otus.processor.Processor;
@@ -19,7 +20,7 @@ public class HomeWork {
       1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
       2. Сделать процессор, который поменяет местами значения field11 и field12
       3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
-            Секунда должна определяьться во время выполнения.
+            Секунда должна определяться во время выполнения.
             Тест - важная часть задания
             Обязательно посмотрите пример к паттерну Мементо!
       4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
@@ -28,33 +29,27 @@ public class HomeWork {
     */
 
     public static void main(String[] args) {
-        // создаём объект field13
         ObjectForMessage ofm = new ObjectForMessage();
         ofm.setData(List.of("X", "Y", "Z"));
 
-        // создаём сообщение с полями 11-13
         Message msg = new Message.Builder(1)
                 .field11("AAA")
                 .field12("BBB")
                 .field13(ofm)
                 .build();
 
-        // создаём процессоры
         Processor swapProcessor = new ProcessorSwapFields11And12();
         Processor exceptionProcessor = new ProcessorEvenSecondException(Clock.systemDefaultZone());
 
         List<Processor> processors = List.of(swapProcessor, exceptionProcessor);
 
-        // создаём ComplexProcessor
         ComplexProcessor complexProcessor = new ComplexProcessor(processors, ex -> {
             System.out.println("Исключение при обработке: " + ex.getMessage());
         });
 
-        // создаём Listener для истории
-        MessageHistoryListener historyListener = new MessageHistoryListener();
+        HistoryListener historyListener = new HistoryListener();
         complexProcessor.addListener(historyListener);
 
-        // обрабатываем сообщение
         try {
             Message result = complexProcessor.handle(msg);
             logger.info("Результат обработки: {}", result);
@@ -62,10 +57,8 @@ public class HomeWork {
             logger.error("Исключение при обработке: {}", e.getMessage());
         }
 
-        // выводим историю сообщений
-        historyListener.getHistory().forEach(m -> logger.info("История сообщения: {}", m));
+        historyListener.findMessageById(msg.getId()).ifPresent(m -> logger.info("История сообщения: {}", m));
 
-        // отключаем listener
         complexProcessor.removeListener(historyListener);
     }
 }
