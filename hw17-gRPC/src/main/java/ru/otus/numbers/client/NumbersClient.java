cc532bfe6bb59ce3a90bdd79f2689aae1199cfee
Natali Korban <natafuuuck@gmail.com>
2025-12-19 23:00:12 +0300

    правки по комментариям

--- hw17-gRPC/src/main/java/ru/otus/numbers/client/NumbersClient.java
+++ hw17-gRPC/src/main/java/ru/otus/numbers/client/NumbersClient.java
@@ -5,14 +5,13 @@ import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class NumbersClient {
     private static final Logger logger = LoggerFactory.getLogger(NumbersClient.class);
 
-    private static int currentValue = 0;
-    private static int lastServerValue = 0;
-    private static boolean newNumberAvailable = false;
-    private static final Object lock = new Object();
+    private static final AtomicLong currentValue = new AtomicLong(0);
+    private static final AtomicLong lastServerValue = new AtomicLong(0);
 
     public static void main(String[] args) throws Exception {
         logger.info("numbers Client is starting...");
@@ -41,24 +40,17 @@ public class NumbersClient {
         }
     }
 
-    public static void updateLastServerValue(int value) {
-        synchronized (lock) {
-            lastServerValue = value;
-            newNumberAvailable = true;
-        }
+    public static void updateLastServerValue(long value) {
+        lastServerValue.set(value);
     }
 
     private static void runClientLoop() {
         new Thread(() -> {
             try {
                 for (int i = 0; i <= 50; i++) {
-                    synchronized (lock) {
-                        if (newNumberAvailable) {
-                            currentValue = currentValue + lastServerValue + 1;
-                            newNumberAvailable = false;
-                        }
-                    }
-                    logger.info("currentValue:{}", currentValue);
+                    // Используем атомарную операцию для обновления currentValue
+                    long newValue = currentValue.addAndGet(lastServerValue.getAndSet(0) + 1);
+                    logger.info("currentValue:{}", newValue);
                     Thread.sleep(1000);
                 }
 
@@ -68,8 +60,7 @@ public class NumbersClient {
                 Thread.currentThread().interrupt();
                 logger.error("Client loop interrupted", e);
             }
-        })
-                .start();
+        }).start();
     }
 
     static class ClientStreamObserver implements StreamObserver<ru.otus.numbers.protobuf.NumberResponse> {
